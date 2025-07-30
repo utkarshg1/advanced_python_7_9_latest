2ff164aa122a647c4f32c814026db89624264ca4
utkarshg1 <gaikwadujg@gmail.com>
2025-07-30 21:02:43 +0530

    variadic

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+def simple_interest(p: float, n: float, r: float) -> tuple[float, float]:
+    i = (p * n * r) / 100
+    a = p + i
+    return i, a
+
+
 def main():
-    print("Hello from adv-python-7-9-pm-ravet!")
+    p = float(input("Enter Principal in INR : "))
+    n = float(input("Enter number of Years : "))
+    r = float(input("Enter Rate of interest in %.p.a. : "))
+    i, a = simple_interest(p, n, r)
+    print(f"Simple Interest : {i:.2f} INR")
+    print(f"Amount : {a:.2f} INR")
 
 
 if __name__ == "__main__":
